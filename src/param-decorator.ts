a1b0c1ebf0cff842a02e4dee8e94ccf5fea467b2
princend <waxdji3g4y3c04@gmail.com>
2020-06-11 15:48:36 +0800

    param-decorator

--- src/param-decorator.ts
+++ src/param-decorator.ts
@@ -1,4 +1,15 @@
 function parameterDecorator(target: any, propertyKey: string, parameterIndex: number) {
-    
-
+    console.log('parameter decorator');
+    console.log(`target:${JSON.stringify(target)}`);
+    console.log(`propertyKey:${propertyKey}`);
+    console.log(`parameterIndex:${parameterIndex}`);
 }
+
+export class Square {
+    remark = '123';
+    getArea(@parameterDecorator radius: number, @parameterDecorator key: number) {
+        return radius ** 2;
+    }
+}
+
+new Square();
